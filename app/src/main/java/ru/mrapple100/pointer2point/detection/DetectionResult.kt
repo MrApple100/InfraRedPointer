6d6089bbcb1c306351ee0c352d69a8cbaba9e8e8
Alexander <mrapple100@yandex.ru>
2022-07-21 01:37:30 +0300

    features for detection

---
+++ app/src/main/java/ru/mrapple100/pointer2point/detection/DetectionResult.kt
@@ -0,0 +1,12 @@
+package ru.mrapple100.pointer2point.detection
+
+import android.graphics.RectF
+
+data class DetectionResult(
+        val x1: Int,
+        val y1: Int,
+        val x2: Int,
+        val y2: Int
+) {
+
+}
